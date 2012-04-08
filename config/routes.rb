e93cc654c64fda462a95c7e04ec16993d19b7950
Tony Xie <txie145@gmail.com>
2012-04-08 16:50:54 -0400

    Finished user signed ups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 AwesomeApp::Application.routes.draw do
-  get "users/new"
-
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
@@ -13,6 +11,8 @@ AwesomeApp::Application.routes.draw do
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+
+  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+ factory :user do 
+ 	name "Tony Xie"
+ 	email "tixe145@gmail.com"
+ 	passowrd "foobar"
+ 	password_confirmation "foobar"
+end
+end
