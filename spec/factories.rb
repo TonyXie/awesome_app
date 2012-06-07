6c63f2b2359cc910814085f7d015fd0f919cdc4d
Tony Xie <txie145@gmail.com>
2012-06-07 09:56:31 -0700

    finisheddd

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
  factory :user do 
- 	name "Tony Xie"
- 	email "txie145@gmail.com"
- 	password "foobar"
- 	password_confirmation "foobar"
+ 	sequence(:name) { |n| "Person #{n}"}
+ 	sequence(:email) {|n| "Person_#{n}@example.com"}
+ 	password "Foobar"
+ 	password_confirmation "Foobar"
 end
 end
